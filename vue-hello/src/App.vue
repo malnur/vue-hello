77a7cc0820345d4853b1b69a5850471c7f4f04ec
Abdul-MAlik MArkabayev <amarkabayev@beeline.kz>
2024-01-21 07:36:21 +0600

    clean code

--- vue-hello/src/App.vue
+++ vue-hello/src/App.vue
@@ -1,25 +1,85 @@
 <script setup>
+import { reactive, ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+const message = ref('Hello Vue.js!')
+
+const headingClass = ref('green')
+
+const type = ref('B')
+const ok = ref(true)
+
+const books = reactive(['book-1', 'book-2', 'book-3', 'book-4'])
+
+const users = reactive([
+  {
+    name: 'User-1',
+    age: 23,
+    books: 3
+  },
+  {
+    name: 'User-2',
+    age: 15,
+    books: 6
+  },
+  {
+    name: 'User-3',
+    age: 40,
+    books: 5
+  }
+])
+
+const count = ref(0)
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <h1>{{ message }}</h1>
-
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
   </header>
 
-  <RouterView />
+  <main>
+    <section>
+      <h2 :class="headingClass">Binding</h2>
+      <p>{{ message.split('').reverse().join('') }}</p>
+    </section>
+
+    <section>
+      <h2>Conditional</h2>
+      <div v-if="type === 'A'">A</div>
+      <div v-else-if="type === 'B'">B</div>
+      <div v-else-if="type === 'C'">C</div>
+      <div v-else>Not A/B/C</div>
+
+      <div v-show="ok">On show message</div>
+    </section>
+
+    <section>
+      <h2>List Items</h2>
+      <ul>
+        <li v-for="(book, index) in books" :key="index" class="green">
+          {{ book }}
+        </li>
+      </ul>
+      <hr />
+      <ol>
+        <li v-for="(user, index) in users" :key="index" class="green">
+          {{ user.name }} has {{ user.books }} books
+        </li>
+      </ol>
+    </section>
+
+    <section>
+      <h2>Event Handler</h2>
+      <div>
+        <button @click="count++">Counter</button>
+        <button @click="count++">Counter</button>
+        <span class="green">{{ count }}</span>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
@@ -33,29 +93,31 @@ header {
   margin: 0 auto 2rem;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
+section {
+  margin-bottom: 1.5rem;
+}
+
+section h2 {
   text-align: center;
-  margin-top: 2rem;
+  margin-bottom: 0.5rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+section hr {
+  margin: 0.5rem 0;
+  border: 1px solid var(--color-text);
 }
 
-nav a.router-link-exact-active:hover {
+section button {
+  padding: 0.5rem 1rem;
+  color: white;
+  border: 1px solid var(--color-text);
+  border-radius: 4px;
   background-color: transparent;
+  margin-right: 1rem;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
+section span {
+  font-size: 1.5rem;
 }
 
 @media (min-width: 1024px) {
@@ -73,15 +135,6 @@ nav a:first-of-type {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
   }
 }
 </style>
